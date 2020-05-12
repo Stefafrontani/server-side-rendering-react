3ca032ef5f933982e95262f7fed5ff952450862f
Stefano Frontani <stefano.frontani@globant.com>
2020-05-12 10:40:32 -0300

    Remove import

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import path from 'path';
 import { renderTemplate } from '../helpers/renderTemplate';
-import { constructURL } from '../helpers/constructURL';
 import React from 'react';
 import ReactDomServer from 'react-dom/server';
 
 const router = express.Router();
 
 router.get('/*', (req, res) => {
-    const view = req.url.replace('/','')
     const viewPathname = req.url.replace('/','')
-
     const capitalizeViewPathname = viewPathname.charAt(0).toUpperCase() + viewPathname.slice(1);
-
     const pathToView = `../../client/pages/${viewPathname}/${capitalizeViewPathname}.jsx`
 
     let ViewFile;
